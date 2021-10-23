8ee81fdc0a9351d540bcd2c72c12aaa1e96bb641
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2021-10-24 01:52:35 +0300

    make static assets work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,4 @@
 import { Component } from '@angular/core';
-//  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-//  <script type="text/javascript" src="../assets/js/tecniqueSelection.js"></script>
-//  <script type="text/javascript" src="./index.bundle.js"></script>
-//  <script type="text/javascript" src="./print.bundle.js"></script>
-//  <script type="text/javascript" src="../assets/js/CamanJS-master/dist/caman.full.min.js"></script>
-//  <script src="../assets/js/chart.min.js"></script>
-//  <script src="../assets/js/bs-init.js"></script>
-//  <script src="../assets/js/imageSelection.js"></script>
-//  <script src="../assets/js/sliderActivity.js"></script>
-//  <script src="../assets/js/theme.js"></script>
 
 @Component({
   selector: 'app-root',
