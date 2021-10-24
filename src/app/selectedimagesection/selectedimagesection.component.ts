771678c0dbe2706adf4494a8dfa017ad6e8ca07e
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2021-10-24 15:11:20 +0300

    it's rendering the image in the canvas

--- src/app/selectedimagesection/selectedimagesection.component.ts
+++ src/app/selectedimagesection/selectedimagesection.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, Directive, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-selectedimagesection',
   templateUrl: './selectedimagesection.component.html',
   styleUrls: ['./selectedimagesection.component.scss']
 })
-export class SelectedimagesectionComponent implements OnInit {
+
+export class SelectedimagesectionComponent implements AfterViewInit {
   @Input() detalizationProperty: any
   @Input() panoramityProperty: any
   @Input() wishToChangeProperty: any
@@ -14,14 +15,49 @@ export class SelectedimagesectionComponent implements OnInit {
   @Input() techniques: any
   @Input() selectedTechniqueIndex: any
 
+  @ViewChild('myCanvas') myCanvas!: ElementRef;
+
+  canvasElement!: HTMLCanvasElement;
+  context!: any
+
   constructor() {
+
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.canvasElement = this.myCanvas.nativeElement;
+    this.context = this.canvasElement.getContext("2d");
+    this.drawImage()
   }
 
   imgSrc(): string {
     return "assets/images/objects/" + this.imageNames[this.selectedImageIndex].filename;
+  }
+
+  drawImage() {
+    var image = new Image();
+    var context = this.context
+    image.src = this.imgSrc();
+    image.addEventListener('load', function(){
+      context.drawImage(image, 0, 0);
+    });
+    image.src = this.imgSrc()
+  }
+
+  detalizationValue(): number {
+    return this.detalizationProperty - 50
+  }
+
+  contrastImage(imgData: any, contrast: number): any{  //input range [-100..100]
+    var d = imgData.data;
+    contrast = (contrast/100) + 1;  //convert to decimal & shift range: [0..2]
+    var intercept = 128 * (1 - contrast);
+    for(var i=0;i<d.length;i+=4){   //r,g,b,a
+      d[i] = d[i]*contrast + intercept;
+      d[i+1] = d[i+1]*contrast + intercept;
+      d[i+2] = d[i+2]*contrast + intercept;
+    }
+    return imgData;
   }
 
 }
