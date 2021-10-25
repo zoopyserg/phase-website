7b700d6e15a6787d8236af9c3949affd91e8e06f
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2021-10-25 15:01:23 +0300

    removed artifacts of older version of the website

--- assets/js/imageSelection.js
+++
@@ -1,9 +0,0 @@
-var imageSelectors = document.getElementsByClassName("imageSelector");
-
-var imageOutput = document.getElementById("displayedObjectImage");
-
-for (var i = 0, len = imageSelectors.length; i < len; i++) {
-  imageSelectors[i].addEventListener("click", function(e) {
-      imageOutput.setAttribute("src", "objects/" + e.target.getAttribute("data-image"));
-    });
-}
